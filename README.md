fa4ffdff6f780d0375aede940a9ffd493a401376
wangrl2016 <wangrl2016@gmail.com>
2024-10-22 18:03:13 +0800

    Define linear function

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Machine Learning Series (机器学习系列)
 
+## 文档：[机器学习 12 天速成](https://docs.google.com/document/d/18V6H_600l-drkXd99pjNtSJtA7rIWWnER-KxIrB-lQY/edit?usp=sharing)
--- crash_course/01_understanding_machine_learning.py
+++ crash_course/01_understanding_machine_learning.py
@@ -2,4 +2,15 @@ import numpy
 from matplotlib import pyplot
 
 if __name__ == '__main__':
-    pass
+    x_values = [0, 5]
+    y_values = [250, 665]
+    x = numpy.linspace(0, 5, 100)
+    y = 83 * x + 250
+    for i, txt in enumerate(zip(x_values, y_values)):
+        pyplot.annotate(f'{txt}', (x_values[i], y_values[i]), textcoords="offset points", xytext=(0, 10))
+    pyplot.text(2.5, 400, 'D(t) = 83t + 250', fontsize=12, color='blue')
+    pyplot.scatter(x_values, y_values, color='red')
+    pyplot.plot(x, y)
+    pyplot.grid(True)
+    pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
+    pyplot.show()
