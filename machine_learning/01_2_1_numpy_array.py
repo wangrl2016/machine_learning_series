c36c76e638cc5e5ffe5f079068da42ebbfd28716
wangrl2016 <wangrl2016@gmail.com>
2024-10-29 22:04:22 +0800

    NumPy array

--- machine_learning/01_2_1_numpy_array.py
+++ machine_learning/01_2_1_numpy_array.py
@@ -2,11 +2,29 @@ import numpy
 
 if __name__ == '__main__':
     a = numpy.array([1, 2, 3, 4, 5, 6])
-    print(a, a[0])
-    a[0] = 10
-    print(a, a[0], a[:3])
-    b = a[3:]
-    print(b)
-    b[0] = 40
-    print(a, b)
+    print(a)
 
+    b = numpy.array([[1, 2], [3, 4]])
+    c = numpy.array([[5, 6]])
+    print(numpy.concatenate((b, c), axis=0))
+
+    d = numpy.array([1, 2, 3, 4, 5, 6])
+    e = d.reshape(3, 2)
+    print(e.shape)
+
+    f = numpy.array([1, 2, 3, 4, 5, 6])
+    g = f[numpy.newaxis, :]
+    print(g.shape)
+
+    h = numpy.array([1, 2, 3])
+    print(h[1], h[0:2], h[1:], h[-2:])
+
+    data = numpy.array([1, 2])
+    ones = numpy.ones(2, dtype=int)
+    print(data + ones)
+
+    data = numpy.array([1.0, 2.0])
+    print(data * 1.6)
+
+    data = numpy.array([1.0, 2.0, 3.0])
+    print(data.max(), data.min(), data.sum())
