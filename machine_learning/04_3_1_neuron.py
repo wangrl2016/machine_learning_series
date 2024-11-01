5cd8dc5541490215d649d32fbbb3c52579d14c3e
wangrl2016 <wangrl2016@gmail.com>
2024-11-01 19:07:14 +0800

    Neuron impl

---
+++ machine_learning/04_3_1_neuron.py
@@ -0,0 +1,20 @@
+import numpy
+
+def sigmoid(x):
+    return 1 / (1 + numpy.exp(-x))
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feedforward(self, inputs):
+        total = numpy.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
+
+if __name__ == '__main__':
+    weights = numpy.array([0, 1])
+    bias = 4
+    neuron = Neuron(weights, bias)
+    x = numpy.array([2, 3])
+    print(neuron.feedforward(x))
