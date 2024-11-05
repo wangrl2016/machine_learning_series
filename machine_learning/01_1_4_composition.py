b751bbf9c8b04433f23767233365fbbb4dc4ca33
wangrl2016 <wangrl2016@gmail.com>
2024-11-05 18:31:47 +0800

    Composition of function

---
+++ machine_learning/01_1_4_composition.py
@@ -0,0 +1,11 @@
+import numpy
+from matplotlib import pyplot
+
+if __name__ == '__main__':
+    x = numpy.linspace(-4, 0, 400)
+    pyplot.plot(x, x * x + 4 * x + 4, label='f(g(x)) = x^2 + 4x + 4')
+    pyplot.plot(x, x * x + 2, label='g(f(x)) = x^2 + 2')
+    pyplot.legend()
+    pyplot.grid(True)
+    pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
+    pyplot.show()
--- machine_learning/01_2_1_numpy_array.py
+++ machine_learning/01_2_1_numpy_array.py
@@ -3,6 +3,9 @@ import numpy
 if __name__ == '__main__':
     a = numpy.array([1, 2, 3, 4, 5, 6])
     print(a)
+    print(numpy.array([[1, 2, 3, 4, 5],
+                       [5, 7, 8, 9, 10],
+                       [11, 12, 13, 14, 15]]))
 
     b = numpy.array([[1, 2], [3, 4]])
     c = numpy.array([[5, 6]])
