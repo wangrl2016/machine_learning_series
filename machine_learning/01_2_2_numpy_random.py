c011949815b6236a9b5f21cc35c4b0fef02a99d3
wangrl2016 <wangrl2016@gmail.com>
2024-11-06 21:29:26 +0800

    Standard normal

--- machine_learning/01_2_2_numpy_random.py
+++ machine_learning/01_2_2_numpy_random.py
@@ -1,4 +1,5 @@
 import numpy
+from matplotlib import pyplot
 
 if __name__ == '__main__':
     rng = numpy.random.default_rng()
@@ -11,3 +12,10 @@ if __name__ == '__main__':
     
     rng = numpy.random.default_rng(seed=0)
     print(rng.random())
+
+    rng = numpy.random.default_rng()
+    data = rng.standard_normal(10000)
+    pyplot.hist(data, bins=50, density=True)
+    pyplot.grid(True)
+    pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
+    pyplot.show()
