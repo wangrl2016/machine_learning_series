6fabfac4d01b4b0ef077dbc4b415e3af26e9c810
wangrl2016 <wangrl2016@gmail.com>
2024-11-06 14:56:01 +0800

    Draw graph of e

--- machine_learning/01_3_2_exponential.py
+++ machine_learning/01_3_2_exponential.py
@@ -3,9 +3,17 @@ from matplotlib import pyplot
 
 if __name__ == '__main__':
     x = numpy.linspace(-4, 4, 800)
-    y = numpy.pow(3, x)
+    y = numpy.power(3, x)
     pyplot.plot(x, y, label='y = 3^x')
     pyplot.legend()
     pyplot.grid(True)
+    pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
+    pyplot.show()
+    
+    n = numpy.linspace(0, 100, 100)
+    m = numpy.power((1 + 1 / n), n)
+    pyplot.plot(n, m, label='m = (1 + 1 / n)^n')
+    pyplot.grid(True)
+    pyplot.legend()
     pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
     pyplot.show()
