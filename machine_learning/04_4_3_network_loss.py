23680c0dd963d24635a443780d031f311ebba0e4
wangrl2016 <wangrl2016@gmail.com>
2024-11-11 18:28:10 +0800

    Network loss

---
+++ machine_learning/04_4_3_network_loss.py
@@ -0,0 +1,31 @@
+import keras
+import numpy
+from matplotlib import pyplot
+
+if __name__ == '__main__':
+    rng = numpy.random.default_rng(seed=0)
+    x = numpy.linspace(0, 10, 100)
+    y = numpy.piecewise(x, [x < 3, (x >= 3) & (x < 7), x >= 7],
+                        [lambda x: x + numpy.random.normal(0, 0.5, len(x)),
+                         lambda x: -0.5 * x + 7 + numpy.random.normal(0, 0.5, len(x)),
+                         lambda x: x - 7 + numpy.random.normal(0, 0.5, len(x))])
+    x = x.reshape(-1, 1)
+    y = y.reshape(-1, 1)
+
+    model = keras.Sequential([
+        keras.layers.Dense(48, activation='relu', input_shape=(1,)),
+        keras.layers.Dense(36, activation='relu'),
+        keras.layers.Dense(24, activation='relu'),
+        keras.layers.Dense(12, activation='relu'),
+        keras.layers.Dense(1)
+    ])
+    optimizer= keras.optimizers.Adam(learning_rate=0.01)
+    model.compile(optimizer=optimizer, loss='mse')
+    history = model.fit(x, y, epochs=100)
+    loss = history.history['loss']
+    
+    pyplot.plot(range(1, 101), loss, label='Loss')
+    pyplot.grid(True)
+    pyplot.legend()
+    pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
+    pyplot.show()
