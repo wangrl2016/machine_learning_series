259dee572a7ddfedf17e0c54197e210ccd050369
wangrl2016 <wangrl2016@gmail.com>
2024-11-16 20:48:43 +0800

    RNN pre process

---
+++ machine_learning/10_1_5_pre_process.py
@@ -0,0 +1,27 @@
+import numpy
+from pos_peg_dataset import train_data, test_data
+
+# Create the vocabulary
+vocab = list(set([w for text in train_data.keys() for w in text.split(' ')]))
+vocab_size = len(vocab)
+print('%d unidque words found' % vocab_size)
+
+# Assign indices to each word.
+word_to_idx = { w: i for i, w in enumerate(vocab) }
+idx_to_word = { i: w for i, w in enumerate(vocab) }
+print(word_to_idx['good'])
+print(idx_to_word[0])
+
+def create_inputs(text):
+    '''
+    Return an array of one-hot vectors representing the words
+    in the input text string.
+    - text is a string
+    - each ont-hot vector has shape (vocab_size, 1)
+    '''
+    inputs = []
+    for w in text.split(' '):
+        v = numpy.zeros((vocab_size, 1))
+        v[word_to_idx[w]] = 1
+        inputs.append(v)
+    return inputs
