5787afc701b1539ef6d7710e8946a45fce7d8b1b
wangrl2016 <wangrl2016@gmail.com>
2024-11-17 09:20:51 +0800

    RNN forward

---
+++ machine_learning/10_1_6_rnn_forward.py
@@ -0,0 +1,49 @@
+import numpy
+from pos_peg_dataset import train_data
+
+def softmax(xs):
+    # Applies the softmax function to the input array.
+    return numpy.exp(xs) / sum(numpy.exp(xs))
+
+
+
+class RNN:
+    # A vanilla recurrent neural network.
+    def __init__(self, input_size, output_size, hidden_size=64):
+        self.Whh = numpy.random.randn(hidden_size, hidden_size) / 1000
+        self.Wxh = numpy.random.randn(hidden_size, input_size) / 1000
+        self.Why = numpy.random.randn(output_size, hidden_size) / 1000
+        self.bh = numpy.zeros((hidden_size, 1))
+        self.by = numpy.zeros((output_size, 1))
+    
+    def forward(self, inputs):
+        # init
+        h = numpy.zeros((self.Whh.shape[0], 1))
+        # Perform each step of the RNN.
+        for _, x in enumerate(inputs):
+            h = numpy.tanh(self.Wxh @ x + self.Whh @ h + self.bh)
+        # Compute the output.
+        y = self.Why @ h + self.by
+        return y, h
+
+if __name__ == '__main__':
+    # Create the vocabulary
+    vocab = list(set([w for text in train_data.keys() for w in text.split(' ')]))
+    vocab_size = len(vocab)
+
+    word_to_idx = { w: i for i, w in enumerate(vocab) }
+    idx_to_word = { i: w for i, w in enumerate(vocab) }
+
+    def create_inputs(text):
+        inputs = []
+        for w in text.split(' '):
+            v = numpy.zeros((vocab_size, 1))
+            v[word_to_idx[w]] = 1
+            inputs.append(v)
+        return inputs
+
+    rnn = RNN(vocab_size, 2)
+    inputs = create_inputs('i am very good')
+    out, h = rnn.forward(inputs)
+    porbs = softmax(out)
+    print(porbs)
