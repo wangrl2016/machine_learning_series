6fb2b35e673b4463e7f84a2635268f1087bd23aa
wangrl2016 <wangrl2016@gmail.com>
2024-11-27 15:53:34 +0800

    Person info data

---
+++ deep_learning/02_4_1_person_info_data.py
@@ -0,0 +1,9 @@
+import numpy
+
+if __name__ == '__main__':
+    person_info_data = numpy.array([
+        [1, 178, 75, 25, 1],
+        [2, 164, 50, 30, 0],
+        [3, 175, 70, 22, 1]
+    ])
+    print(person_info_data.shape)
