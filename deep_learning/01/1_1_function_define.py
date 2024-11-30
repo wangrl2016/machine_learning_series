5cb218824b3dd416d31175cbaa421832f0cbdb68
wangrl2016 <wangrl2016@gmail.com>
2024-11-30 20:16:38 +0800

    Function define

--- deep_learning/01/1_1_function_define.py
+++ deep_learning/01/1_1_function_define.py
@@ -4,6 +4,10 @@ if __name__ == '__main__':
     x = [1, 2, 3, 4, 5]
     y = [2, 4, 6, 8, 10]
     pyplot.scatter(x, y, marker='o')
+    # 标记每个点的坐标
+    for i in range(len(x)):
+        pyplot.annotate(f'({x[i]}, {y[i]})', (x[i], y[i]),
+                        textcoords='offset points', xytext=(5, 5), ha='center')
     pyplot.grid(True)
     pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
     pyplot.show()
