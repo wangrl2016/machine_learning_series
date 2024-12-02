efb6a541bd96a7cfbb57319d62763682f07a7904
wangrl2016 <wangrl2016@gmail.com>
2024-12-02 22:20:19 +0800

    Quadratic vertex

--- deep_learning/01/1_4_quadratic_vertex.py
+++ deep_learning/01/1_4_quadratic_vertex.py
@@ -1,3 +1,11 @@
+from matplotlib import pyplot
+import numpy
 
 if __name__ == '__main__':
-    pass
+    x = numpy.linspace(-5, 1)
+    pyplot.plot(x, -3 * (x + 2)**2 + 4, label='y = -3(x+2)^2 + 4')
+    pyplot.axvline(x=-2, linestyle='--', label='x = -2')
+    pyplot.grid(True)
+    pyplot.legend()
+    pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
+    pyplot.show()
---
+++ deep_learning/01/1_5_rate_change.py
@@ -0,0 +1,5 @@
+from matplotlib import pyplot
+import numpy
+
+if __name__ == '__main__':
+    pass
