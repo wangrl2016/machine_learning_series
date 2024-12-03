a8a2a39cc7ae7ab9d16764364e5e7ad7b37dbd19
wangrl2016 <wangrl2016@gmail.com>
2024-12-03 10:43:36 +0800

    Rate change

--- deep_learning/01/1_5_rate_change.py
+++ deep_learning/01/1_5_rate_change.py
@@ -2,4 +2,9 @@ from matplotlib import pyplot
 import numpy
 
 if __name__ == '__main__':
-    pass
+    x = numpy.linspace(-5, 10, 1500)
+    pyplot.plot(x, x**3 - 6 * x**2 - 15 * x + 20, label='y = x^3 - 6x^2 - 15x + 20')
+    pyplot.grid(True)
+    pyplot.legend()
+    pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
+    pyplot.show()
