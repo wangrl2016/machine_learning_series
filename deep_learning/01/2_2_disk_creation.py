adb1ab45f1cb46d2b9317e19384a8dfbc510c880
wangrl2016 <wangrl2016@gmail.com>
2024-12-07 18:41:12 +0800

    Load csv file

--- deep_learning/01/2_2_disk_creation.py
+++ deep_learning/01/2_2_disk_creation.py
@@ -4,6 +4,10 @@ import pandas
 file_path = 'temp/personal_info.xlsx'
 
 if __name__ == '__main__':
+    arr = numpy.loadtxt('res/deep_learning/simple_int.csv',
+                        delimiter=',', skiprows=1)
+    print(arr)
+
     df = pandas.DataFrame({
         "Number": numpy.array([1, 2, 3]),
         "Age": numpy.array([25, 30, 22])
