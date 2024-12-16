169a590f58c2cebdf1820e4da5007cb8e1ad289c
wangrl2016 <wangrl2016@gmail.com>
2024-12-16 10:30:59 +0800

    BinaryCrossentropy loss

---
+++ deep_learning/03/4_4_bce_loss.py
@@ -0,0 +1,11 @@
+import keras
+import numpy
+
+if __name__ == '__main__':
+    # 真实标签
+    y_true = numpy.array([1.0, 0.0, 1.0, 1.0])
+    # 预测值
+    y_pred = numpy.array([0.9, 0.1, 0.8, 0.7])
+
+    loss = keras.losses.BinaryCrossentropy()
+    print(loss(y_true, y_pred).numpy())
