4994bbafa1979d9818dc53b24cafa319095d3461
wangrl2016 <wangrl2016@gmail.com>
2024-12-29 11:52:19 +0800

    Sigmoid function

--- deep_learning/04/3_1_sigmoid_function.py
+++ deep_learning/04/3_1_sigmoid_function.py
@@ -10,6 +10,12 @@ def deriv_sigmoid(x):
 if __name__ == '__main__':
     x = numpy.linspace(-10, 10, 400)
     pyplot.plot(x, sigmoid(x), label='f(x) = 1 / (1 + e^-x)')
+    pyplot.legend()
+    pyplot.grid(True)
+    pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
+    pyplot.show()
+
+    pyplot.plot(x, sigmoid(x), label='f(x) = 1 / (1 + e^-x)')
     pyplot.plot(x, deriv_sigmoid(x), label = 'f\'(x) = f(x) * (1 - f(x))')
     pyplot.legend()
     pyplot.grid(True)
