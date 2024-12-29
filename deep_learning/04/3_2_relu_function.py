ee88c0cb790c8cbecad5ca35890d27abe21b9fa5
wangrl2016 <wangrl2016@gmail.com>
2024-12-29 21:42:30 +0800

    ReLU function

--- deep_learning/04/3_2_relu_function.py
+++ deep_learning/04/3_2_relu_function.py
@@ -10,6 +10,11 @@ def deriv_relu(x):
 if __name__ == '__main__':
     x = numpy.linspace(-5, 5, 1000)
     pyplot.plot(x, relu(x), label='f(x) = x > 0 ? x : 0')
+    pyplot.legend()
+    pyplot.grid(True)
+    pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
+    pyplot.show()
+    pyplot.plot(x, relu(x), label='f(x) = x > 0 ? x : 0')
     pyplot.plot(x, deriv_relu(x), label='f\'(x) = x > 0 ? 1 : 0')
     pyplot.legend()
     pyplot.grid(True)
