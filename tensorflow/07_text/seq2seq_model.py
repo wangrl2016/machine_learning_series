e141deedd0c29dff6ea303ed6d4f9d38e3a97910
wangrl2016 <wangrl2016@gmail.com>
2025-02-08 11:27:57 +0800

    Decoder layer

--- tensorflow/07_text/seq2seq_model.py
+++ tensorflow/07_text/seq2seq_model.py
@@ -141,6 +141,73 @@ class CrossAttention(keras.layers.Layer):
         return x
 
 
+class Decoder(keras.layers.Layer):
+    @classmethod
+    def add_method(cls, fun):
+        setattr(cls, fun.__name__, fun)
+        return fun
+    
+    def __init__(self, text_processor, units):
+        super(Decoder, self).__init__()
+        self.text_processor = text_processor
+        self.vocab_size = text_processor.vocabulary_size()
+        self.word_to_id = keras.layers.StringLookup(
+            vocabulary=text_processor.get_vocabulary(),
+            mask_token='', oov_token='[UNK]')
+        self.id_to_word = keras.layers.StringLookup(
+            vocabulary=text_processor.get_vocabulary(),
+            mask_token='', oov_token='[UNK]',
+            invert=True)
+        self.start_token = self.word_to_id('[START]')
+        self.end_token = self.word_to_id('[END]')
+        
+        self.units = units
+        
+        # 1. The embedding layer converts token IDs to vectors.
+        self.embedding = keras.layers.Embedding(self.vocab_size,
+                                                units,
+                                                mask_zero=True)
+        # 2. The RNN keeps track of what's been generated so far.
+        self.rnn = keras.layers.GRU(units,
+                                    return_sequences=True,
+                                    return_state=True,
+                                    recurrent_initializer='glorot_uniform')
+        # 3. The RNN output will be the query for the attention layer.
+        self.attention = CrossAttention(units)
+        
+        # 4. This fully connected layer produces the logits for each output token.
+        self.output_layer = keras.layers.Dense(self.vocab_size)
+
+@Decoder.add_method
+def call(self, context, x, state=None, return_state=False):
+    shape_checker = ShapeChecker()
+    shape_checker(x, 'batch t')
+    shape_checker(context, 'batch s units')
+    
+    # 1. Lookup the embeddings
+    x = self.embedding(x)
+    shape_checker(x, 'batch t units')
+    
+    # 2. Process the target sequence.
+    x, state = self.rnn(x, initial_state=state)
+    shape_checker(x, 'batch t units')
+    
+    # 3. Use the RNN output as the query for the attention over the context.
+    x = self.attention(x, context)
+    self.last_attention_weights = self.attention.last_attention_weights
+    shape_checker(x, 'batch t units')
+    shape_checker(self.last_attention_weights, 'batch t s')
+    
+    # 4. Generate logit predictions for the next token.
+    logits = self.output_layer(x)
+    shape_checker(logits, 'batch t target_vocab_size')
+    
+    if return_state:
+        return logits, state
+    else:
+        return logits
+
+
 if __name__ == '__main__':
     print(tf.__version__)
     path_to_zip = keras.utils.get_file(
@@ -273,3 +340,9 @@ if __name__ == '__main__':
     pyplot.title('Mask')
     pyplot.subplots_adjust(left=0.08, right=0.92, top=0.94, bottom=0.06)
     pyplot.show()
+    
+    decoder = Decoder(target_text_processor, UNITS)
+    logits = decoder(ex_context, ex_tar_in)
+    print(f'encoder output shape: (batch, s, units) {ex_context.shape}')
+    print(f'input target tokens shape: (batch, t) {ex_tar_in.shape}')
+    print(f'logits shape shape: (batch, target_vocabulary_size) {logits.shape}')
