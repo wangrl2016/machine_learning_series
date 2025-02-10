82ae468b05a21a26b64842cb4566a2e3525fb2d9
wangrl2016 <wangrl2016@gmail.com>
2025-02-10 18:57:10 +0800

    MNIST for expert

--- tensorflow/01_quickstart/2_1_quickstart_expert.py
+++ tensorflow/01_quickstart/2_1_quickstart_expert.py
@@ -1,3 +1,85 @@
+import keras
+import tensorflow as tf
+
+class MyModel(keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = keras.layers.Conv2D(32, 3, activation='relu')
+        self.flatten = keras.layers.Flatten()
+        self.d1 = keras.layers.Dense(128, activation='relu')
+        self.d2 = keras.layers.Dense(10)
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        return self.d2(x)
 
 if __name__ == '__main__':
-    pass
+    print('Keras version:', keras.__version__)
+    print('TensorFlow version:', tf.__version__)
+
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    # Add a channels dimension.
+    x_train = x_train[..., tf.newaxis].astype('float32')
+    x_test = x_test[..., tf.newaxis].astype('float32')
+
+    BATCH_SIZE = 32
+
+    train_ds = tf.data.Dataset.from_tensor_slices(
+        (x_train, y_train)).shuffle(10000).batch(BATCH_SIZE)
+    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(BATCH_SIZE)
+
+    # Create an instance of the model.
+    model = MyModel()
+
+    loss_object = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    optimizer = keras.optimizers.Adam()
+
+    train_loss = keras.metrics.Mean(name='train_loss')
+    train_accuracy = keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+    test_loss = keras.metrics.Mean(name='test_loss')
+    test_accuracy = keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+    @tf.function
+    def train_step(images, labels):
+        with tf.GradientTape() as tape:
+            predictions = model(images, training=True)
+            loss = loss_object(labels, predictions)
+
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(labels, predictions)
+
+    @tf.function
+    def test_step(images, labels):
+        predictions = model(images, training=False)
+        t_loss = loss_object(labels, predictions)
+
+        test_loss(t_loss)
+        test_accuracy(labels, predictions)
+
+    EPOCHS = 5
+    for epoch in range(EPOCHS):
+        # Reset the metrics at the start of the next epoch
+        train_loss.reset_state()
+        train_accuracy.reset_state()
+        test_loss.reset_state()
+        test_accuracy.reset_state()
+
+        for images, labels in train_ds:
+            train_step(images, labels)
+
+        for test_images, test_labels in test_ds:
+            test_step(test_images, test_labels)
+
+        print(
+            f'Epoch {epoch + 1}, '
+            f'loss: {train_loss.result():0.2f}, '
+            f'accuracy: {train_accuracy.result() * 100:0.2f}, '
+            f'test loss: {test_loss.result():0.2f}, '
+            f'test accuracy: {test_accuracy.result() * 100:0.2f}')
