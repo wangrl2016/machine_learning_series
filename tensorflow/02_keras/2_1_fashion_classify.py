a3c37ab6d8f7b5b7ce460b8649777e3aa79adaf8
wangrl2016 <wangrl2016@gmail.com>
2025-02-11 10:47:37 +0800

    Preprocess the data

--- tensorflow/02_keras/2_1_fashion_classify.py
+++ tensorflow/02_keras/2_1_fashion_classify.py
@@ -17,3 +17,26 @@ if __name__ == '__main__':
     print(train_labels[:10])
     print(test_images.shape)
     print(len(test_labels))
+
+
+    pyplot.imshow(train_images[0])
+    pyplot.colorbar()
+    pyplot.grid(False)
+    pyplot.subplots_adjust(left=0.08, right=0.98, top=0.96, bottom=0.06)
+    pyplot.show()
+
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+
+    for i in range(25):
+        pyplot.subplot(5, 5, i+1)
+        pyplot.xticks([])
+        pyplot.yticks([])
+        pyplot.grid(False)
+        pyplot.imshow(train_images[i], cmap=pyplot.cm.binary)
+        pyplot.xlabel(class_names[train_labels[i]])
+    pyplot.subplots_adjust(wspace=0.5, hspace=0.5,
+                           left=0.04, right=0.96, top=0.96, bottom=0.06)
+    pyplot.show()
+
+
