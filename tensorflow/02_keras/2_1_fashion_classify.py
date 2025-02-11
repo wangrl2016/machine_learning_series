48778f9d886555584f863fa92ddd41495729c074
wangrl2016 <wangrl2016@gmail.com>
2025-02-11 10:33:22 +0800

    Explore the data

--- tensorflow/02_keras/2_1_fashion_classify.py
+++ tensorflow/02_keras/2_1_fashion_classify.py
@@ -8,4 +8,12 @@ if __name__ == '__main__':
     print('TensorFlow version:', tf.__version__)
 
     (train_images, train_labels), (test_images, test_labels) = keras.datasets.fashion_mnist.load_data()
+    
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+    print(train_images.shape)
+    print(len(train_labels))
+    print(train_labels[:10])
+    print(test_images.shape)
+    print(len(test_labels))
