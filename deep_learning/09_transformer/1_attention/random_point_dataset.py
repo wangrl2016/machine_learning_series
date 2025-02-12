b28f27870bbda38d46787607fe8ceb7a4d481cef
wangrl2016 <wangrl2016@gmail.com>
2025-02-12 16:50:59 +0800

    Random point dataset

--- deep_learning/09_transformer/1_attention/random_point_dataset.py
+++ deep_learning/09_transformer/1_attention/random_point_dataset.py
@@ -9,14 +9,12 @@ if __name__ == '__main__':
     n_train = 50
     x_train = numpy.sort(rng.random(n_train) * 5)
     y_train = func(x_train) + rng.normal(0.0, 0.5, (n_train,))
-    x_test = numpy.arange(0, 5, 0.1)
-    y_test = func(x_test)
-    n_test = len(x_test)
+    x_truth = numpy.arange(0, 5, 0.1)
+    y_truth = func(x_truth)
     
     pyplot.plot(x_train, y_train, 'o', alpha=0.5, label='Samples')
-    pyplot.plot(x_test, y_test, label='True')
+    pyplot.plot(x_truth, y_truth, label='Truth')
     pyplot.grid(True)
     pyplot.legend()
     pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
     pyplot.show()
-    
